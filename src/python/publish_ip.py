ad0dec00eb8e5a6946bb9d3a03004b0cbecebcb2
Heiko Kromer <h@heikokromer.com>
2022-09-16 19:41:19 +0200

    added publish ip

--- src/python/publish_ip.py
+++ src/python/publish_ip.py
@@ -13,15 +13,13 @@ bash_get_wlan = f'/home/{username}/hamsterwheel/src/bash/get_wlan.sh'
 subprocess.call(['sh', bash_get_wlan])
 
 # Sleep for a few seconds to allow bash script to retrieve wlan information
-time.sleep(5)
+time.sleep(3)
 
 # Load the wlan information from file
 ifconfig_file = f'/home/{username}/ifconfig.txt'
-file_content = []
+
 with open(ifconfig_file, 'r') as file:
-    for line in file:
-       if 'wlan0' in line:
-            file_content.append(line)
+    file_content = file.readlines
     file.close()
 
 print(file_content)
