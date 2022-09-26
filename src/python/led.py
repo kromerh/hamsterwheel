642f48f334390f36517005e12c7bf29aa74e24ea
kromerh <HeikoKromer@user-goldens-MacBook-Pro.local>
2022-09-26 18:33:39 +0200

    test led

--- src/python/led.py
+++ src/python/led.py
@@ -4,8 +4,11 @@ import time
 LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
-GPIO.output(LED_PIN, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(LED_PIN, GPIO.LOW)
+cnt = 0
+while cnt < 10:
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_PIN, GPIO.LOW)
+    cnt += 1
 
 GPIO.cleanup()
