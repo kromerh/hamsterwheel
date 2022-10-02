115d08f6f639fdfaacdd8a2a200b3e43bc6ad885
Heiko Kromer <h@heikokromer.com>
2022-10-02 14:57:19 +0200

    publish ip

--- src/python/constants.py
+++ src/python/constants.py
@@ -25,9 +25,15 @@ LOG_HAMSTERWHEEL = f'{HOME}{LOGS}{FILENAME_LOG_HAMSTERWHEEL}'
 LOG_PUBLISHIP = f'{HOME}{LOGS}{FILENAME_LOG_PUBLISHIP}'
 
 # AWS
-AWS_CLIENT_NAME = "rpi_hamsterwheel_sensor"
+# AWS_CLIENT_NAME = "rpi_hamsterwheel_sensor"
+# AWS_ENDPOINT = "a72qba275aic3-ats.iot.eu-central-1.amazonaws.com"
+# AWS_CA_FILE = "/home/wilson/certificates/AmazonRootCA1.pem"
+# AWS_KEY = "/home/wilson/certificates/private-key.pem.key"
+# AWS_CERT = "/home/wilson/certificates/device-certificate.pem.crt"
+# AWS_TOPIC = "topic/wilson"
+AWS_CLIENT_NAME = "done2"
 AWS_ENDPOINT = "a72qba275aic3-ats.iot.eu-central-1.amazonaws.com"
-AWS_CA_FILE = "/home/wilson/certificates/AmazonRootCA1.pem"
-AWS_KEY = "/home/wilson/certificates/private-key.pem.key"
-AWS_CERT = "/home/wilson/certificates/device-certificate.pem.crt"
-AWS_TOPIC = "topic/wilson"
+AWS_CA_FILE = "/home/ertificates/AmazonRootCA1.pem"
+AWS_KEY = "/home/certificates/private.pem.key"
+AWS_CERT = "/home/certificates/certificate.pem.crt"
+AWS_TOPIC = "topic/done2"
