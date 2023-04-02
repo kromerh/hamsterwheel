2e4957c0ba3dc2a8e69d59e5473e795afbc17784
kromerh <HeikoKromer@tiberius.home>
2023-04-02 18:29:21 +0200

    updated code

--- src/python/constants.py
+++ src/python/constants.py
@@ -1,6 +1,6 @@
 """
 ================================================================================
-Author:      Heiko Kromer - 2022
+Author:      Heiko Kromer - 2023
 Description: This script contains constants used in hamsterwheel.py
 ================================================================================
 """
@@ -8,7 +8,7 @@ Description: This script contains constants used in hamsterwheel.py
 _REMOTE = False
 
 # RPi paths
-HOME = '/home/done1/'
+HOME = '/home/wilson/'
 REPO = 'hamsterwheel'
 LOGS = '/logs/'
 
@@ -25,15 +25,9 @@ LOG_HAMSTERWHEEL = f'{HOME}{LOGS}{FILENAME_LOG_HAMSTERWHEEL}'
 LOG_PUBLISHIP = f'{HOME}{LOGS}{FILENAME_LOG_PUBLISHIP}'
 
 # AWS
-# AWS_CLIENT_NAME = "rpi_hamsterwheel_sensor"
-# AWS_ENDPOINT = "a72qba275aic3-ats.iot.eu-central-1.amazonaws.com"
-# AWS_CA_FILE = "/home/wilson/certificates/AmazonRootCA1.pem"
-# AWS_KEY = "/home/wilson/certificates/private-key.pem.key"
-# AWS_CERT = "/home/wilson/certificates/device-certificate.pem.crt"
-# AWS_TOPIC = "topic/wilson"
-AWS_CLIENT_NAME = "done1"
+AWS_CLIENT_NAME = "rpi_hamsterwheel_sensor"
 AWS_ENDPOINT = "a72qba275aic3-ats.iot.eu-central-1.amazonaws.com"
-AWS_CA_FILE = "/home/certificates/AmazonRootCA1.pem"
-AWS_KEY = "/home/certificates/private.pem.key"
-AWS_CERT = "/home/certificates/certificate.pem.crt"
-AWS_TOPIC = "topic/done1"
+AWS_CA_FILE = "/home/wilson/certificates/AmazonRootCA1.pem"
+AWS_KEY = "/home/wilson/certificates/private-key.pem.key"
+AWS_CERT = "/home/wilson/certificates/device-certificate.pem.crt"
+AWS_TOPIC = "topic/wilson"
